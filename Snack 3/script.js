2cae0ff58b51b8d67e7dba11979e3699c5f4c0bb
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-08-27 12:02:59 +0100

    fix

--- Snack 3/script.js
+++ Snack 3/script.js
@@ -5,6 +5,6 @@ var cognomi = ["Del Piero", "Batistuta", "Baggio"];
 // Gatsby vuole generare una falsa lista di 3 invitati.
 listautenti = [];
 for (var i = 0; i < 3; i++) {
-    listautenti.push(nomi[Math.floor(Math.random() * 2)] +  " " + cognomi[Math.floor(Math.random() * 2)]); 
+    listautenti.push(nomi[Math.floor(Math.random() * 3)] +  " " + cognomi[Math.floor(Math.random() * 3)]); 
 }
 alert("La lista utenti è composta da: " + listautenti);
